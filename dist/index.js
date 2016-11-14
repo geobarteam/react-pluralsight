809bd04e2b11bbf2a001e1d6a7239294cf22601f
geobarteam <gvd8@hotmail.com>
2016-11-14 14:08:11 +0100

    added missing types

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ var ReactDOM = require("react-dom");
 var react_router_1 = require('react-router');
 var route_1 = require('./route');
 function render() {
-    ReactDOM.render(React.createElement(react_router_1.Router, {history: react_router_1.browserHistory}, route_1.default), document.getElementById('app'));
+    ReactDOM.render(React.createElement(react_router_1.Router, {history: react_router_1.browserHistory, routes: route_1.default}), document.getElementById('app'));
 }
 ;
 render();
